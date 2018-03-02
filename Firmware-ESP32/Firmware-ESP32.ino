e834934986cf2dee0e5fdfd7ef9917d3081b49c0
Dillon <ee.tinkerer@gmail.com>
2018-03-01 21:36:16 -0500

    connect to NTP server and get time and print to display

--- Firmware-ESP32/Firmware-ESP32.ino
+++ Firmware-ESP32/Firmware-ESP32.ino
@@ -1,12 +1,49 @@
 #include "display.h"
+// WiFi settings:
+// (refer to credentials-example.h for assistance)
+#include "credentials.h"
+#include <WiFi.h>
+#include "time.h"
 
-int time = 0;
+const char * ntpServer = "pool.ntp.org";
+const long gmtOffset_sec = 3600;
+const int daylightOffset_sec = 3600;
+
+int unix_time = 0;
+
+void printLocalTime() {
+  struct tm timeinfo;
+  if (!getLocalTime( & timeinfo)) {
+    Serial.println("Failed to obtain time");
+    return;
+  }
+  unix_time = time(NULL);
+  Serial.println(unix_time);
+}
 
 void setup() {
+  Serial.begin(115200);
+
+  //connect to WiFi
+  Serial.printf("Connecting to %s ", SSID);
+  WiFi.begin(SSID, PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println(" CONNECTED");
+
+  //init and get the time
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  printLocalTime();
+
+  //disconnect WiFi as it's no longer needed
+  WiFi.disconnect(true);
+  WiFi.mode(WIFI_OFF);
+
   displaySetup();
 }
 
 void loop() {
-  displayTime(time/1000);
-  time++;
+  displayTime(time(NULL));
 }
