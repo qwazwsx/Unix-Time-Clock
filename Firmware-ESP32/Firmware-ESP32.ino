44c87bfe489542afb19a16c58a515f2b611cf7c5
Dillon <ee.tinkerer@gmail.com>
2018-03-05 11:24:10 -0500

    can remove initial getTime

--- Firmware-ESP32/Firmware-ESP32.ino
+++ Firmware-ESP32/Firmware-ESP32.ino
@@ -3,7 +3,10 @@
 
 void setup() {
   // get initial time from internet
-  getTimeFromInternet();
+  // should now automatically do this inside setTime()
+  // if it doesn't, create a function to set refresh to 10
+  // (so it would have to be different)
+  //getTimeFromInternet();
 
   // set up the display pins
   displaySetup();
