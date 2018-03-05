2cade3073df39dd424a7d885933c2df464e94eee
Dillon <ee.tinkerer@gmail.com>
2018-03-05 10:38:02 -0500

    move getTimeFromInternet() into it's own files

--- Firmware-ESP32/Firmware-ESP32.ino
+++ Firmware-ESP32/Firmware-ESP32.ino
@@ -1,38 +1,5 @@
 #include "display.h"
-// WiFi settings:
-// (refer to credentials-example.h for assistance)
-#include "credentials.h"
-#include <WiFi.h>
-#include "time.h"
-
-const char * ntpServer = "pool.ntp.org";
-const long gmtOffset_sec = 3600;
-const int daylightOffset_sec = 3600;
-
-void getTimeFromInternet(void) {
-  //connect to WiFi
-  //Serial.printf("Connecting to %s ", SSID);
-  WiFi.begin(SSID, PASSWORD);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(250);
-    //Serial.print(".");
-  }
-  //Serial.println(" CONNECTED");
-
-  //init and get the time
-  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-
-  struct tm timeinfo;
-  if (!getLocalTime( & timeinfo)) {
-    //Serial.println("Failed to obtain time");
-    return;
-  }
-  //Serial.println(unix_time);
-
-  //disconnect WiFi as it's no longer needed
-  WiFi.disconnect(true);
-  WiFi.mode(WIFI_OFF);
-}
+#include "getTime.h"
 
 void setup() {
   //Serial.begin(115200);
---
+++ Firmware-ESP32/getTime.h
@@ -0,0 +1,17 @@
+#ifndef gettime_h
+#define gettime_h
+
+//#include <WiFi.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// for unix time:
+void getTimeFromInternet(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
